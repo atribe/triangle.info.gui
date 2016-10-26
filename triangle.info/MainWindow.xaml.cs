68ec52168b3cb6a3ed23c8604a8550f3ff8a48e8
Allan <atribe13@gmail.com>
2016-10-26 12:21:02 -0600

    Created the triangle class. GUI is in a grid.

--- triangle.info/MainWindow.xaml.cs
+++ triangle.info/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace triangle.info
 {
@@ -20,9 +21,36 @@ namespace triangle.info
     /// </summary>
     public partial class MainWindow : Window
     {
+        Triangle tri;
+
         public MainWindow()
         {
             InitializeComponent();
+            tbxSideA.TextChanged += new TextChangedEventHandler(sideChanged);
+            tbxSideB.TextChanged += new TextChangedEventHandler(sideChanged);
+            tbxSideC.TextChanged += new TextChangedEventHandler(sideChanged);
+
+            tri = new Triangle(tbxSideA.Text, tbxSideB.Text, tbxSideC.Text);
+        }
+
+        private void sideChanged(object sender, TextChangedEventArgs e)
+        {
+            tri.updateSides(tbxSideA.Text, tbxSideB.Text, tbxSideC.Text);
+
+            if(tri.isValidTriangle())
+            {
+
+            }
+            else
+            {
+                infoOutput.Content = "These side lengths do not produce a valid triangle";
+            }
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
---
+++ triangle.info/Triangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangle.info
+{
+    class Triangle
+    {
+        private double sideA { get; set; }
+        private double sideB { get; set; }
+        private double sideC { get; set; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        }
+
+        public Triangle(string stringSideA, string stringSideB, string stringSideC)
+        {
+            this.sideA = Convert.ToDouble(stringSideA);
+            this.sideB = Convert.ToDouble(stringSideB);
+            this.sideC = Convert.ToDouble(stringSideC);
+        }
+
+        internal void updateSides(string stringSideA, string stringSideB, string stringSideC)
+        {
+            this.sideA = Convert.ToDouble(stringSideA);
+            this.sideB = Convert.ToDouble(stringSideB);
+            this.sideC = Convert.ToDouble(stringSideC);
+        }
+
+        public bool isValidTriangle()
+        {
+            double[] sideArray = new double[] { sideA, sideB, sideC };
+            Array.Sort<double>(sideArray);
+
+            bool returnMe = false;
+
+            if(sideArray[0] + sideArray[1] > sideArray[2])
+            {
+                returnMe = true;
+            }
+            else
+            {
+                returnMe = false;
+            }
+
+            return returnMe;
+        }
+
+        
+    }
+}
